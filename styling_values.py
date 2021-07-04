895901703625dc97904533127d4ab46fd29c098f
Sebastian <sebastian.meckovski@gmail.com>
2021-07-04 14:02:35 +0100

    Separated some styling values into a separate file

---
+++ styling_values.py
@@ -0,0 +1,13 @@
+style = {
+    'border': '#9db0ae solid 2px',
+    'border-radius': '10px',
+    'background-color': '#bfd8d5',
+    'padding': '3px'}
+
+dropdown_style = {
+    'color': 'black',
+    'background-color': '#dfdfdf',
+    'border-radius': '7px'
+}
+
+bar_color = '#5f8580'
